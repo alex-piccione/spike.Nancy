2890b52befc9b10aaaed1717cb0c2d8902111785
alex <apiccione@sportingsolutions.com>
2020-01-03 12:56:59 +0000

    Create a demo Nancy project.

---
+++ Weather API/WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace Weather_API
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32 + (int (float this.TemperatureC / 0.5556))
